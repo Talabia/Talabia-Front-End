2917662e525331347001634fabc4264ba813b96d
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-09-28 08:34:13 +0300

    add new components and services

---
+++ src/app/features/looksup/vehicle-makers/vehicle-makers.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-makers',
+  imports: [],
+  templateUrl: './vehicle-makers.component.html',
+  styleUrl: './vehicle-makers.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VehicleMakersComponent { }
---
+++ src/app/features/looksup/vehicle-models/vehicle-models.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-vehicle-models',
+  imports: [],
+  templateUrl: './vehicle-models.component.html',
+  styleUrl: './vehicle-models.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class VehicleModelsComponent { }
