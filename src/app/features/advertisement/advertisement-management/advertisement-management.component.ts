4e6f7627541c8a004029239971089cc229b0515e
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-10-01 12:43:27 +0300

    set new components

---
+++ src/app/features/advertisement/advertisement-management/advertisement-management.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisement-management',
+  imports: [],
+  templateUrl: './advertisement-management.component.html',
+  styleUrl: './advertisement-management.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class AdvertisementManagementComponent { }
---
+++ src/app/features/users/user-account-management/user-account-management.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-account-management',
+  imports: [],
+  templateUrl: './user-account-management.component.html',
+  styleUrl: './user-account-management.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class UserAccountManagementComponent { }
