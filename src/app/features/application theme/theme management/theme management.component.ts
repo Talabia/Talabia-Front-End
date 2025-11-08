52cd7e83896d697f0b1070caa9ec9e2cada36483
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-08 13:12:10 +0200

    set theme component

---
+++ src/app/features/application theme/theme management/theme management.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-management',
+  imports: [],
+  templateUrl: './theme management.component.html',
+  styleUrl: './theme management.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ThemeManagementComponent { }
