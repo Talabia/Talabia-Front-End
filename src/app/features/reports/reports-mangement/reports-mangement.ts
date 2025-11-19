1c5e1424178424baa5f238fe96694e8c1f4390ed
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-11-19 04:27:31 +0200

    set new components

---
+++ src/app/features/reports/reports-mangement/reports-mangement.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-reports-mangement',
+  imports: [],
+  templateUrl: './reports-mangement.html',
+  styleUrl: './reports-mangement.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ReportsMangement { }
