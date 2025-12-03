0ac125af6412ddd6dd5e120316c2992eb0f9ed58
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2025-12-03 20:36:59 +0200

    set new page component

---
+++ src/app/features/content-mangement/content-mangement/content-mangement.component.ts
@@ -0,0 +1,10 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-mangement',
+  imports: [],
+  templateUrl: './content-mangement.component.html',
+  styleUrl: './content-mangement.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ContentMangementComponent {}
